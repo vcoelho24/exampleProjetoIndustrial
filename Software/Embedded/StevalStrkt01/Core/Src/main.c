63623c4489494acf764b44d9bf59fef53b052776
Jorge Silva <jhgsilva@ua.pt>
2020-04-10 01:19:23 +0100

    Rm

--- Software/Embedded/StevalStrkt01/Core/Src/main.c
+++ Software/Embedded/StevalStrkt01/Core/Src/main.c
@@ -102,14 +102,14 @@ int main(void)
 
   /* USER CODE END 2 */
  
- 
+
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
